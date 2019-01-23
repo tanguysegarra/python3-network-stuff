a69af023ea6f1f1f251c1fe5a8747f3394717399
Tanguy Segarra <tanguy.segarra@epita.fr>
2019-01-23 10:25:01 +0100

    added '/killserv' command from client to shutdown the server

--- echo-server/server.py
+++ echo-server/server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import socket                                         
+
+import socket
+import sys
 
 def startServer():
 
@@ -10,13 +12,20 @@ def startServer():
     servSock.bind((host, port))
 
     servSock.listen(5)
+    print("Waiting for a connection...")
 
     while 42:
         cltSock, addr = servSock.accept()
         print("Got a connection from %s" % str(addr))
 
         msg = cltSock.recv(1024).decode('ascii')
-        print("Received: " + msg)
+        
+        if msg == "/killserv":
+            print("Shutting down server...")
+            cltSock.send("Server killed".encode('ascii'))
+            sys.exit(0)
+        
+        print("Received: " + msg)            
 
         echo = "Thank you for connecting!" + "\n"
         cltSock.send(echo.encode('ascii'))
